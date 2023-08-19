5e07534d13bb1b713d2dc9393de8dcb8b43f3323
HappyProgger <zoisberger@mail.ru>
2023-08-19 21:44:27 +0300

    Laravel Vue board

---
+++ resources/js/src/App.vue
@@ -0,0 +1,80 @@
+
+<template>
+    <main class="main">
+
+        <div class="content">
+            <p class="main_text" >Ответы на вопросы</p>
+
+          <div  v-for="post in posts" :key="post.answer">
+                <questions_component :answer="post.main_question" :main_question="post.answer"></questions_component>
+
+          </div>
+
+
+
+
+        </div>
+    </main>
+
+</template>
+
+
+
+<script>
+    import questions_component from "./components/question.vue"
+
+
+    export default {
+        components : { questions_component },
+        data(){
+            return {
+                posts : [
+                    { answer : 'Lorem ipsum dolor sit amet.\n', main_question : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non sapien vel.'},
+                    { answer : 'Lorem ipsum dolor sit amet.\n', main_question : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices magna ut ' +
+                            'luctus cursus. Aliquam elementum tortor at enim venenatis, eu dapibus nulla gravida. Suspendisse consectetur erat at ligula mollis varius.' +
+                            ' Quisque vel dignissim ante. Sed efficitur, dui nec suscipit.'},
+                    { answer : 'Lorem ipsum dolor sit amet.\n', main_question : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non sapien vel.'},
+
+                ]
+            }
+        }
+    }
+
+</script>
+
+<style>
+html *{
+    box-sizing: border-box ;
+    background-color: #d7d0d0;
+    color: black;
+}
+
+.main_text{
+    font-size: 3em;
+}
+main *{
+    height: 100%;
+    width: 100%;
+
+}
+
+.content{
+    margin: 40px 50px 20px 50px;
+    width: inherit;
+
+}
+
+@media screen and (min-width: 1440px){
+
+.content{
+    margin: 70px 100px;
+
+}
+
+
+
+
+}
+
+
+</style>
---
+++ resources/js/src/components/question.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="part">
+        <div class="header">{{ main_question }}<button @click="is_open_arrow_active ? this.is_open_arrow_active = false :this.is_open_arrow_active = true"
+                                                     :class="is_open_arrow_active ? 'open_arrow_none_active':'open_arrow_active' "></button></div>
+
+        <div class="answer" :style=" is_open_arrow_active ? 'display: none;' :  ''">
+            {{answer}}
+        </div>
+    </div>
+
+</template>
+
+<script>
+ export default {
+     name : "questions_component",
+     props :  {
+         'main_question' : {
+             required: true,
+             type: String
+         },
+         'answer' : {
+             required: true,
+             type : String
+         }},
+
+     data(){
+         return {
+            is_open_arrow_active : true,
+
+         };
+     },
+
+ }
+</script>
+
+<style>
+
+.header{
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    font-size: 2em;
+    margin-bottom: 4vh;
+
+}
+.answer{
+    width: inherit;
+    max-width: 1000px;
+    //display: ;
+
+}
+.part{
+    //min-height: 250px;
+    width: inherit;
+    margin-bottom: 5vh;
+}
+
+.open_arrow_active{
+    justify-items: end;
+    margin-left: 1vw;
+    height: 2.5em;
+    width: 2.5em;
+    border-style: none;
+    background: url("../../../../resources/img/svg/downarrow_120663.svg") no-repeat;
+    background-size: contain;
+    transform: rotate(180deg);
+}
+
+
+.open_arrow_none_active{
+    justify-items: end;
+    margin-left: 1vw;
+    height: 2.5em;
+    width: 2.5em;
+    border-style: none;
+    background: url("../../../../resources/img/svg/downarrow_blue.svg") no-repeat;
+    background-size: cover;
+    //transform: rotate(180deg);
+    fill: blue;
+}
+
+
+
+
+</style>
